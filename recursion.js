d4e7a40a60f9aafe7157e5652c38d30912f0edb1
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-11-07 11:34:15 -0500

    Add recursive function to generate a fibonacci sequence of n length

--- recursion.js
+++ recursion.js
@@ -12,4 +12,26 @@ function fibs(number) {
     }
     return sequence;
 }
-console.log(fibs(8)); // [0, 1, 1, 2, 3, 5, 8, 13]
+// console.log(fibs(8)); // [0, 1, 1, 2, 3, 5, 8, 13]
+
+function fibsRec(number) {
+    let fibSeqence;
+    let prevNumber;
+    let nextNumber;
+    let currentNumber; 
+
+    if (number === 0) {
+        return [0];
+    } else if (number === 1) {
+        return [0,1];
+    } else if (number >= 2) {
+        fibSeqence = fibsRec(number - 1);
+        prevNumber = fibSeqence.length - 2;
+        nextNumber = fibSeqence.length - 1;
+        currentNumber = prevNumber + nextNumber; 
+        fibSeqence.push(currentNumber);
+    }
+
+    return fibSeqence;
+}
+// console.log(fibsRec(22)); // [0, 1, 1, 2, 3, 5, 8, 13]
